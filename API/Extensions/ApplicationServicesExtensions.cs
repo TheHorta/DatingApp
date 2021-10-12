942c0ed50859c9a1b454415e70bb01150a701586
Chris Griffin <CGriffin@equitable.ca>
2021-10-12 07:37:03 -0400

    End of section 11

--- API/Extensions/ApplicationServicesExtensions.cs
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -18,8 +18,11 @@ namespace API.Extensions
         public static IServiceCollection AddApplicationServices(this IServiceCollection services,
                                                                 IConfiguration config)
         {
+            services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
             services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IPhotoService, PhotoService>();
             services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IPhotoService, PhotoService>();
             services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly); //so automapper can find profiles
             services.AddDbContext<DataContext>(options =>
             {
---
+++ API/Helpers/CloudinarySettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Helpers
+{
+    public class CloudinarySettings
+    {
+        public string ApiKey { get; set; }
+        public string ApiSecret { get; set; }
+        public string Cloudname { get; set; }
+    }
+}
